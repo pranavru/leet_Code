824c4b4615d953147fe09f81d88ead7fa5ecd218
Pranav Ruparelia <pranavruparelia9@gmail.com>
2020-09-29 22:05:59 -0400

    Updated values based on multiple of 2

--- PowerOfANumber.js
+++ PowerOfANumber.js
@@ -7,10 +7,10 @@ var myPow = function(x, n) {
     if(n === 0) return 1; 
     else if( n === 1 ) return x;
     else {
-        if(n < 0) return 1/ myPow(x, n * -1)
+        if(n < 0) return myPow(1/x, n * -1)
         
-        // (n & 1) === 0 :- Checks if number is multiple of 2 or not
-        return (n & 1) === 0 ? myPow(x **2, n >>> 1) : x * myPow(x **2, n >> 1);        
+        // Checks if number is multiple of 2 or not
+        return (n %2) === 0 ? myPow(x **2, n /2) : x * myPow(x, n -1);        
     }
 };
 
